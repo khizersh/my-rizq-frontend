83bb0dfd9cd5024bbb7c2304dbc01e6262ba9602
Muhammad khizer <khizer.younus@simpaisa.com>
2023-01-17 15:47:53 +0500

    base url

--- src/utility.js
+++ src/utility.js
@@ -1,2 +1,2 @@
-export var BASE_URL = BASE_URL + "https://myrizq-backend.onrender.com";
+export var BASE_URL =  "https://myrizq-backend.onrender.com";
 // export var BASE_URL = "http://localhost:3001";
