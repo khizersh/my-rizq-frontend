8b2a9ca5f6587edd26759f4f200671fa7215f42c
khizersh <moh.khizer61@gmail.com>
2023-03-18 14:06:04 +0500

    vps work

--- src/utility.js
+++ src/utility.js
@@ -1,4 +1,5 @@
-export var BASE_URL = "https://myrizq-backend.onrender.com";
+// export var BASE_URL = "https://myrizq-backend.onrender.com";
+export var BASE_URL = "http://178.128.236.228:3001";
 // export var BASE_URL = "http://localhost:3001";
 // export var BASE_URL = "http://192.168.168.56:3001";
 // import "./assets"
